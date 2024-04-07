997a894b6afe175c0eca5cce3c0d6067a5049f12
vj-sonawane <vijayshreeacademy20@gmail.com>
2024-04-08 00:46:43 +0530

    Coloured log and Logger log in log file

--- src/main/java/com/dps/usercrud/repository/UserRegistrationRepository.java
+++ src/main/java/com/dps/usercrud/repository/UserRegistrationRepository.java
@@ -14,14 +14,14 @@ import java.util.List;
 @Transactional
 public interface UserRegistrationRepository extends JpaRepository<UserDetails, Integer>{
 
-    //Named Parameter
+
     @Query(nativeQuery = true,
             value = "select * from userregistration.user_details ud where ud.user_city=:userCity")
-    @Comment("This query retrieve the user details based on City")
+    @Comment("This query retrieve the user details based on City using Named parameter")
     List<UserDetails> findUsersWithCity(@Param("userCity") String city);
 
-    //Positional Parameter
     @Query(nativeQuery = true,
             value = "SELECT * FROM userregistration.user_details ud WHERE ud.user_city = ?1 AND ud.user_gender = ?2")
+    @Comment("This query retrieve the user details based on City and Gender using Positional parameter")
     List<UserDetails> findByCityAndGenderPositionalBind(String city, String gender);
 }
